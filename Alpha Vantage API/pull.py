63a08405dd99d23695d9afdeca4fed1b055b4782
BenTrope <benjamintrollope@gmail.com>
2020-08-17 12:47:20 +0200

    successful daily bitcoin query

--- Alpha Vantage API/pull.py
+++ Alpha Vantage API/pull.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 
 import pandas
-from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
 import sys
 import random
 
 #read ticker argument
 ticker = str(sys.argv[1])
-#mrkt = 'USD'
+mrkt = 'USD'
 
 
 #multiple keys allow for higher rate of access to requests from free tier accounts
 lines = open('keys').read().splitlines()
 keys = random.choice(lines)
 
-time= TimeSeries(key=keys, output_format = 'pandas')
-data = time.get_intraday( symbol= ticker , interval = '1min', outputsize = 'full')
-#data = get_digital_currency_daily 
+time= CryptoCurrencies(key=keys, output_format = 'pandas')
+#data = time.get_intraday( symbol= ticker , interval = '1min', outputsize = 'full')
+data = time.get_digital_currency_daily(symbol = ticker , market =mrkt )
 
 
 
