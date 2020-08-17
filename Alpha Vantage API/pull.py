03c682f13a865a6b297278ae50fa60b6af7af5a9
BenTrope <benjamintrollope@gmail.com>
2020-08-17 17:24:08 +0200

    Testing graph output

--- Alpha Vantage API/pull.py
+++ Alpha Vantage API/pull.py
@@ -4,19 +4,23 @@
 sources 
 https://www.youtube.com/watch?v=339AfkUQ67o
 https://www.youtube.com/watch?v=JJO9fKj3_u4
+https://www.youtube.com/watch?v=T2mQiesnx8s
+https://www.youtube.com/watch?v=T2mQiesnx8s
 
 '''
 
 import pandas as pd
+import numpy as np
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
 from alpha_vantage.techindicators import TechIndicators
 import matplotlib.pyplot as plt
 import sys
 import random
 
-#read ticker argument
+#Variables
 ticker = str(sys.argv[1])
 mrkt = 'USD'
+period = 7
 
 
 #multiple keys allow for higher rate of access to requests from free tier accounts
@@ -25,24 +29,42 @@ keys = random.choice(lines)
 
 #pull data
 time= CryptoCurrencies(key=keys, output_format = 'pandas')
-data_ts, meta_data_ts = time.get_digital_currency_daily(symbol = ticker , market =mrkt )
+data_ts , meta_data_ts = time.get_digital_currency_daily(symbol = ticker , market =mrkt )
 
-
-
-period = 7 
-#df1 = data_ts['4b. close (USD)'].iloc[period-1::]
-#simple moving average// ie rolling average
-
-ti = TechIndicators(key= keys ,output_format= 'pandas')
-data_ti, meta_data_ti = ti.get_sma(symbol = 'BTC' , time_period=period,
-                                    interval= 'daily', series_type= 'close')
 
 
 
 #pulling closing values from time series 
-df_closing = data_ts['4b. close (USD)']
+#df_close = data_ts['4b. close (USD)']
+df_close = pd.DataFrame(data_ts.iloc[:, 2])
+
+
+#renaming column
+df_close.columns = ['BTC_Daily_Close_USD']
+
+
+#create 7 & 21 day rolling average
+df_close['MA_7'] = df_close.BTC_Daily_Close_USD.rolling(7).mean()
+df_close['MA_21'] = df_close.BTC_Daily_Close_USD.rolling(21).mean()
+
+
+#plotting
+
+plt.figure(figsize=(15,20))
+plt.grid(True)
+plt.plot(df_close['BTC_Daily_Close_USD'], label ='BTC')
+plt.plot(df_close['MA_7'], label ='MA_7')
+plt.plot(df_close['MA_21'], label ='MA_21')
+plt.legend(loc=2)
+
+
+
+
+
+
 
 
 
 print(ticker)
-print(df_closing)
+print(df_close.columns)
+print(df_close)
